04052efb596794cf7d182033769b921f722d1b4d
nishantniranjan <nishant.niranjan@gmail.com>
2014-08-02 20:56:34 +0530

    application context impl

--- ApplicationCtx/src/main/java/com/persistent/App.java
+++ ApplicationCtx/src/main/java/com/persistent/App.java
@@ -16,12 +16,19 @@ public class App
     	Triangle triangle = (Triangle)factory.getBean("triangle");
     	triangle.draw();
         System.out.println( "Hello World!" + triangle.getArea() );
-        System.out.println("triangle::"+triangle);
+        System.out.println("triangle1::"+triangle);
+        triangle.setArea(1000);
+        System.out.println("triangle2::"+triangle);
+        Triangle triangle1=(Triangle)factory.getBean("triangle");
+        //System.out.println( "Hello World::" + triangle.getArea() );
+        System.out.println("triangle3::"+triangle1);
         Rectangle rectangle =(Rectangle) factory.getBean("rectangle");
         System.out.println("Rectangle::"+rectangle);
         rectangle =(Rectangle) factory.getBean("rectangle1");
         System.out.println("Rectangle1::"+rectangle);
         Line line =(Line)factory.getBean("line");
         System.out.println("Lines::"+line);
+        Square square =(Square)factory.getBean("square");
+        System.out.println("Square::"+square);
     }
 }
